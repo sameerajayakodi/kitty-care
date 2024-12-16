0b042df45de194e7d8c88d94cc4f05ee8c38b97e
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-12-16 18:54:15 +0530

    update HelloApplication

--- src/main/java/org/samee/lk/skypos/HelloApplication.java
+++ src/main/java/org/samee/lk/skypos/HelloApplication.java
@@ -10,10 +10,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("product-add-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("dashboard-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1900, 1000);
         stage.setOpacity(1);
-        stage.setTitle("Petcare System");
+        stage.setTitle("KittyCare");
         stage.setScene(scene);
         stage.show();
     }
