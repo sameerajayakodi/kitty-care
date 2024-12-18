2633bfccb1e0b1c9ebf6162e429acb7666a16a99
Sameera Jayakodi <sameerajayakodi456@gmail.com>
2024-12-18 16:26:40 +0530

    update project

--- src/main/java/org/samee/lk/skypos/HelloApplication.java
+++ src/main/java/org/samee/lk/skypos/HelloApplication.java
@@ -1,9 +1,11 @@
 package org.samee.lk.skypos;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.awt.*;
 import java.io.IOException;
@@ -13,7 +15,10 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("checkout-view/checkout-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1900, 1000);
-        stage.setOpacity(1);
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1), scene.getRoot());
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
 
         stage.setTitle("KittyCare");
         stage.setScene(scene);
